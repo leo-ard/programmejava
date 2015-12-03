7e77ce6efa551b4b8534c61abc44d316ecfb29c5
lool01 <lool4516@gmail.com>
2015-12-02 21:28:11 -0500

    Ajout du système de sauvegarde Ajout de la fenetre Ajout dy thread

--- Jeux/src/GamePane.java
+++ Jeux/src/GamePane.java
@@ -1,12 +1,95 @@
+import java.awt.Dimension;
 
-public class GamePane {
-	String shit;
+import javax.swing.JPanel;
+
+public class GamePane extends JPanel implements Runnable{
 	
-	public GamePane(int shit){
-		this.shit = "yo face goddammit";
-		System.out.print(shit);
+	private static final long serialVersionUID = 1204454795981689683L;
 	
+	//----FIELDS----// 
+	
+	//essentials
+	private Thread thread;
+	
+	//frame
+	private final int FPS = 30;
+	private double averageFPS = 0;
+
+	public GamePane(){
+		super();
+		setPreferredSize(new Dimension(WIDTH,HEIGHT));
+		setFocusable(true);
 		
+		requestFocus();
+	}
+	
+	public void initiate(){
+		//all fields for the game will be initiate here
+	}
+	
+	public void addNotify(){
+		super.addNotify();
+		if(thread == null){
+			thread = new Thread(this);
+			thread.start();
+		}
+		//Initiate keylisteners here
+	}
+	
+	
+
+	public void run() {
+		
+		long startTime = 0;
+		long URDTimeMillis;
+		long waitTime;
+		long totalTime = 0;
+		
+		int frameCount = 0;
+		int maxFrameCount = 30;
+		
+		long targetTime = 1000/FPS;
+		
+		while(true){
+			
+			
+			
+			
+			
+			//---- System for the fps ----//
+			
+			URDTimeMillis = (System.nanoTime() - startTime) / 1000000;
+			
+			waitTime = targetTime - URDTimeMillis;
+			if(waitTime <0){waitTime = 0;}
+			
+			
+				
+			
+			try {
+				Thread.sleep(waitTime);
+			} catch (InterruptedException e) {}
+			
+			totalTime += System.nanoTime() - startTime;
+			frameCount++;
+			
+			if(frameCount == maxFrameCount){
+				averageFPS = 1000.0 / ((totalTime / frameCount)/1000000);
+				frameCount = 0;
+				totalTime = 0;
+			}
+		}
+		
+	}
+	
+	//---- getter and setter ----//
+
+	public double getAverageFPS() {
+		return averageFPS;
+	}
+
+	public void setAverageFPS(double averageFPS) {
+		this.averageFPS = averageFPS;
 	}
 
 }
--- Jeux/src/Main.java
+++ Jeux/src/Main.java
@@ -1,11 +1,27 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class Main {
 	
+	public static JFrame windows;
+	public static GameSave gameSave;
+	
 	public static void main(String Args[]){
 		
-		System.out.println("allo");
+		windows = new JFrame();
+		windows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		windows.setResizable(true);
+		windows.setMinimumSize(new Dimension(760,650));
 		
-		System.out.println("bob");
+		gameSave = new GameSave();
+		gameSave.load();
+		
+		windows.setContentPane(new GamePane());
+		
+		windows.pack();
+		windows.setLocationRelativeTo(null);
+		windows.setVisible(true);
 		
 	}
 
